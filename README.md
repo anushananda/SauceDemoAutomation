4287c3504d71806f9528b6f37e406b7d3a0a9b9f
Anusha <anusha.nanda7@gmail.com>
2025-08-16 01:58:07 +0530

    Updated POM base and other files

--- README.md
+++ README.md
@@ -4,12 +4,23 @@
 Automated test scripts for SauceDemo (https://www.saucedemo.com/) using Selenium, Java, TestNG, and Maven.
 
 ## Requirements
-- Java 11
-- Maven
-- TestNG
-- Firefox Driver
+- Java 11.0.16(JDK 11)
+- Maven 3.9.9
+- TestNG 7.11.0
+- GeckoDriver (Firefox)
+- An IDE (Eclipse )
+- Windows 10
+- Git
+
+## Project Structure 
+src/test/java         -> Contains both Page Object classes and Test classes  
+testng.xml            -> Test suite configuration  
+README.md             -> Project documentation  
+test-output/          -> TestNG HTML reports  
+pom.xml               -> pom.xml -> Maven dependencies & build config
+  
 
 ## How to run
 1. Clone this repo:
    ```bash
-   git clone https://github.com/yourusername/SauceDemoAutomation.git
+   git clone https://github.com/anushananda/SauceDemoAutomation.git
--- src/test/java/com/saucedemo/base/Base.java
+++ src/test/java/com/saucedemo/base/Base.java
@@ -2,10 +2,13 @@ package com.saucedemo.base;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import com.saucedemo.pages.Login;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 
 
@@ -13,14 +16,17 @@ import com.saucedemo.pages.Login;
 public class Base {
 	protected static WebDriver driver;
 	
-	String path = "D:\\All_Material_selenium\\Automation\\Drivers\\geckodriver-v0.35.0-win-aarch64\\geckodriver.exe";
+	//String path = "D:\\Automation\\Drivers\\geckodriver-v0.35.0-win-aarch64\\geckodriver.exe";
 	String url= "https://www.saucedemo.com";
 	
 	
 	@BeforeSuite
 	public void setupLogin() {
-	System.setProperty("webdriver.firefox.driver", path);
+	WebDriverManager.firefoxdriver().setup();
 	driver = new FirefoxDriver();
+		
+	//System.setProperty("webdriver.firefox.driver", path);
+	//driver = new FirefoxDriver();
 	driver.get(url);
 	driver.manage().window().maximize(); 
 	driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
